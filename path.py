af524b12fc881a7b7a7533486240f452479633fe
Fernando Rodriguez <fer.rodza@gmail.com>
2024-07-11 02:44:28 -0600

    Maze solver 0.3

--- path.py
+++ path.py
@@ -1,44 +1,41 @@
 import numpy as np
+import time
 
-p_y, i_y = 6 , 6 # Posicion inicial - Initial position
+p_y, i_y = 13 , 13 # Posicion inicial - Initial position
 p_x, i_x = 0 , 0
-path = [(p_y, p_x)] # Variable donde guardar el camino final - Variable to store final path
-visited = [(p_y, p_x)] # Variable para guardar casillas visitadas - Variable to store visited cells
+#path = [(p_y, p_x)] # Variable donde guardar el camino final - Variable to store final path
+
+visited = [] # Variable para guardar casillas visitadas - Variable to store visited cells
+possible_directions = []
 
 flag = False # Bandera de detención - Detention flag
 goal = False
 
-# maze = np.array([
-#     [1, 1, 1, 1, 1, 1, 2],      # 0
-#     [1, 0, 0, 0, 1, 0, 0],      # 1
-#     [1, 1, 0, 1, 1, 1, 0],      # 2
-#     [0, 1, 1, 1, 1, 1, 0],      # 3
-#     [1, 1, 0, 1, 1, 1, 1],      # 4
-#     [0, 1, 0, 1, 0, 0, 1],      # 5
-#     [1, 1, 0, 1, 0, 1, 1]       # 6
-#     #0  1  2  3  4  5  6
-# ])
-
 maze = np.array([
-    [1, 1, 1, 1, 0, 1, 1],
-    [1, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 2, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 0, 1, 1, 1]
-])
+    [1, 1, 1, 1, 1, 1, 1],      # 0
+    [1, 0, 0, 1, 0, 0, 1],      # 1
+    [1, 1, 0, 1, 1, 0, 1],      # 2
+    [1, 0, 0, 2, 0, 0, 1],      # 3
+    [1, 0, 1, 0, 1, 0, 1],      # 4
+    [1, 0, 1, 1, 0, 0, 1],      # 5
+    [0, 1, 1, 1, 1, 1, 1],       # 6
+    [0, 0, 0, 1, 0, 0, 1],      # 0
+    [1, 0, 1, 1, 1, 0, 1],      # 1
+    [1, 0, 1, 1, 0, 1, 1],      # 2
+    [1, 0, 1, 1, 0, 0, 1],      # 3
+    [1, 1, 1, 1, 1, 0, 1],      # 4
+    [1, 0, 0, 1, 0, 0, 1],      # 5
+    [1, 1, 1, 1, 1, 1, 1] 
+    #0  1  2  3  4  5  6
+    ])
+
 
 directions = [
     (1, 0),  # Abajo - Down
-    #(-1, 0), # Arriba - Up
+    (-1, 0), # Arriba - Up
     (0, 1),  # Derecha - Rigth
     (0, -1),  # Izquierda - Left
-    (-1, 0)
     ]
-
-checkpoints =[]
-possible_directions = []
 
 def imprimir_laberinto(y, x, laberinto, path):
     laberinto[y, x] = 3
@@ -55,11 +52,12 @@ while flag == False:
     # Recorrer cada dirección
     for dy, dx in directions:
         new_y, new_x = p_y + dy, p_x + dx
-        # Verificar si la nueva posición está dentro de los límites
         if 0 <= new_y < maze.shape[0] and 0 <= new_x < maze.shape[1]:
             if maze[new_y, new_x] == 2:
-                path.append((new_y, new_x))
+                visited.append((new_y, new_x))
                 goal = True
+                print("Completed")
+                print(visited)
                 #if checkpoint len == 0: flag = True
                 # if possible_directions == []:
                 #     flag = True
@@ -67,39 +65,38 @@ while flag == False:
                 #     break
             
             if maze[new_y, new_x] == 1:
-                # if checar si ya está en path
-                possible_directions.append((new_y, new_x))
+                if (new_y, new_x) not in possible_directions:
+                    possible_directions.append((new_y, new_x))
 
-    for (y, x) in possible_directions:
-            if (y, x) in visited:
-                possible_directions.remove((y, x))  # Si ya visitó la casilla, eliminar esa como posible camino a tomar
+    visited.append((p_y, p_x))
+
+    possible_directions = [(y, x) for (y, x) in possible_directions if (y, x) not in visited] # Improved possible directions
 
     print(possible_directions)
+    print(p_y, p_x)
+    #print(visited)
     
     if len(possible_directions) == 1:
 
         p_y = possible_directions[0][0]
         p_x = possible_directions[0][1]
 
-        try: 
-            if path_1 == []: #Si existe y no está vacio
-                path_1.append((p_y, p_x))
-            else:
-                path.append((p_y, p_x))
-        except:
-            pass
+        # path.append((p_y, p_x))
 #        path.append((p_y, p_x))
         
         visited.append((p_y, p_x))
-        
-        possible_directions = []
+        possible_directions.remove((p_y, p_x))
         
         print(p_y, p_x, " ==1 ")
+
+        if goal:
+            print(visited)
+
+        print(" ////////////// ")
 
 
     elif len(possible_directions) > 1:
 
-        visited.append((p_y, p_x))
 
         #checkpoints.append((p_y, p_x)) #guarda esa posicion para regresar despeus
         
@@ -107,11 +104,16 @@ while flag == False:
         p_x = possible_directions[0][1]
 
        # possible_directions = []   ### CHECAR ESTO BIEN
-
-        path_1 = []
-        path_1.append((p_y, p_x))
-        visited.append((p_y, p_x))
+        possible_directions.remove((p_y, p_x))
+        # path_1 = []
+        # path_1.append((p_y, p_x))
+        #visited.append((p_y, p_x))
         print(p_y, p_x, " > 1 ")
+        
+        if goal:
+            print(visited)
+        
+        print(" ////////////// ")
     
     elif len(possible_directions) == 0:
 
@@ -125,16 +127,14 @@ while flag == False:
        # checkpoints.pop()
         print("xd: ", possible_directions)
         print(p_y,", " , p_x, " == 0 ")
-        #regresar a checkpoint
-        #eliminar checkpoint y path_1
+        print(" ////////////// ")
+
 
         #if possible directions > 1, y,x append checkpoint, elegir una 
         # pass
+    #time.sleep(0.2) # just for debugging
 
 imprimir_laberinto(i_y, i_x, maze, visited[1:])
 
-print("Path: ", path)
-print("Path 1: ", path_1)
 print("visited: ", visited)
-print("possible ", possible_directions)
-#print("check: ", checkpoints)
+print("possible: ", possible_directions)
